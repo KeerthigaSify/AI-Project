a7430669c987044009f7d2e9e0ac25d9cfa60735
KeerthigaSify <keerthiga.panneerselvam@sifycorp.com>
2026-03-20 13:08:36 +0530

    Add files via upload

---
+++ XxsifyLeaveTypeLovVORowImpl.java
@@ -0,0 +1,66 @@
+/*===========================================================================+
+ | Sify Technologies – Employee Leave System                                 |
+ | LOV View Object Row : XxsifyLeaveTypeLovVORowImpl                        |
+ +==========================================================================*/
+package oracle.apps.sfc.empleavesystem.lov.server;
+
+import oracle.apps.fnd.framework.server.OAViewRowImpl;
+
+import oracle.jbo.server.AttributeDefImpl;
+
+public class XxsifyLeaveTypeLovVORowImpl extends OAViewRowImpl {
+
+    public static final int MEANING = 0;
+    public static final int LOOKUPCODE = 1;
+
+    /**This is the default constructor (do not remove)
+     */
+    public XxsifyLeaveTypeLovVORowImpl() {
+    }
+
+    public String getMeaning()    { return (String) getAttributeInternal(MEANING); }
+    public String getLookupCode() { return (String) getAttributeInternal(LOOKUPCODE); }
+
+    /**Sets <code>value</code> as the attribute value for the calculated attribute Meaning
+     */
+    public void setMeaning(String value) {
+        setAttributeInternal(MEANING, value);
+    }
+
+    /**Sets <code>value</code> as the attribute value for the calculated attribute LookupCode
+     */
+    public void setLookupCode(String value) {
+        setAttributeInternal(LOOKUPCODE, value);
+    }
+
+    /**getAttrInvokeAccessor: generated method. Do not modify.
+     */
+    protected Object getAttrInvokeAccessor(int index, 
+                                           AttributeDefImpl attrDef) throws Exception {
+        switch (index) {
+        case MEANING:
+            return getMeaning();
+        case LOOKUPCODE:
+            return getLookupCode();
+        default:
+            return super.getAttrInvokeAccessor(index, attrDef);
+        }
+    }
+
+    /**setAttrInvokeAccessor: generated method. Do not modify.
+     */
+    protected void setAttrInvokeAccessor(int index, Object value, 
+                                         AttributeDefImpl attrDef) throws Exception {
+        switch (index) {
+        case MEANING:
+            setMeaning((String)value);
+            return;
+        case LOOKUPCODE:
+            setLookupCode((String)value);
+            return;
+        default:
+            super.setAttrInvokeAccessor(index, value, attrDef);
+            return;
+        }
+    }
+}
